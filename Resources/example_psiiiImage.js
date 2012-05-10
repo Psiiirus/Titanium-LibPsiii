49bb5cacbc0b0bf64065988812a40b9b387e2922
toni <tg@mandarin-medien.de>
2012-05-10 09:34:04 +0200

    added example how to use the onResizeCallback

--- Resources/example_psiiiImage.js
+++ Resources/example_psiiiImage.js
@@ -20,16 +20,43 @@ var UI = {
 													);
 	$win.add($imageView_width);
 
+	/*
+	 * example how to use the onResizeFinish - Callback
+	 * shows how to build an flexible UI
+	 */
+
+
+	// Create a Label.
+	var $movingTestLabel = Ti.UI.createLabel({
+		text : 'This label will be placed at the right bottom of the image if the image is fully rendered',
+		textAlign : 'left',
+		width: 'auto',
+		height: 'auto',
+		bottom : 0,
+		left : 0,
+		backgroundColor: 'red'
+	});
+	
+	// Add to the parent view.
+	$win.add($movingTestLabel);
+	 
 	var $imageView_height = UI.PsiiiImage.createResizedImage('http://www.androidguys.com/wp-content/uploads/2009/08/appcelerator1.png',
 														{
 															
 															height : 100, //image gets resized to fit a height of 200
-															
 															borderWidth: 5,
 															borderColor:'#FFF',
 															top : 120,
 															left: 30
-														}
+														},//image constructor paramerters
+														function(_resizedImageBlob,_constructorParams)
+														{
+															var $x = _resizedImageBlob.width + _constructorParams.left;
+															var $y = _resizedImageBlob.height + _constructorParams.top;
+															alert("_resizedImageBlob "+_resizedImageBlob.height+" _constructorParams " + _constructorParams.top);
+															$movingTestLabel.bottom = null;
+															$movingTestLabel.animate({ top: $y,left:$x, duration: 500 });
+														}//image finished resizing callback
 													);
 	$win.add($imageView_height);
 	
